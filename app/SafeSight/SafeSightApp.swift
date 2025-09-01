d7b74d202802aaa139c066779cb9aeef6446e663
MattCarvajal <ma449997@ucf.edu>
2025-08-31 22:34:36 -0400

    made app folder and pushed my app stuff MC

---
+++ app/SafeSight/SafeSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeSightApp.swift
+//  SafeSight
+//
+//  Created by Matt Carvajal on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SafeSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app/SafeSightTests/SafeSightTests.swift
@@ -0,0 +1,17 @@
+//
+//  SafeSightTests.swift
+//  SafeSightTests
+//
+//  Created by Matt Carvajal on 1/23/25.
+//
+
+import Testing
+@testable import SafeSight
+
+struct SafeSightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
