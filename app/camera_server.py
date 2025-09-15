3f21f212e6a3258dbf5b8f8f25d5e15d7e68d0e8
MattCarvajal <matt2carvajal5@gmail.com>
2025-09-15 18:42:01 +0100

    added photo pull to the app MC

--- app/camera_server.py
+++ app/camera_server.py
@@ -2,13 +2,16 @@
 # SafeSight Senior Design
 # Written by Matthew Carvajal
 
-from flask import Flask, send_file
+from flask import Flask, send_file, send_from_directory, jsonify
 from picamera2 import Picamera2
 import time
+import os
 
 app = Flask(__name__)
 camera = Picamera2()
 camera.configure(camera.create_still_configuration())
+
+PHOTO_DIR = os.path.expanduser("~/Desktop/test_photos") # Directory for pictures
 
 # /capture path to capture a picture
 @app.route("/capture")
@@ -24,6 +27,16 @@ def capture():
 @app.route("/ping")
 def ping():
     return {"status": "ok"}
+
+@app.route("/photos")
+def list_photos():
+    files = sorted(os.listdir(PHOTO_DIR))
+    files = [f for f in files if f.lower().endswith((".jpg", ".jpeg", ".png"))]
+    return jsonify({"photos": files})
+
+@app.route("/photos/<filename>")
+def get_photo(filename):
+    return send_from_directory(PHOTO_DIR, filename)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080) #wlan0 on pi
