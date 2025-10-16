ab6b1a94fdd67393dd7c95c9dece728a6520ca07
MattCarvajal <matt2carvajal5@gmail.com>
2025-10-16 22:05:44 +0100

    added updated CV files MC

--- computerVision/parallelCV.py
+++ computerVision/parallelCV.py
@@ -1,17 +1,37 @@
 import cv2
 import dlib
 import numpy as np
+import os
+import time
+from datetime import datetime
 from PIL import Image
 from ultralytics import YOLO
+import serial, time
+
+# Initiate serial 
+ser = serial.Serial('/dev/serial0', 115200, timeout=1)
 
 # Load the detector and predictor
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
+def send_bit(b):
+    ser.write(bytes([b]))
+    ser.flush()
+    print("Sent", b)
+
 # Load a pretrained YOLOv8n model
 model = YOLO('best_traffic_nano_yolo.pt')
 
+# path for pictures
+save_path = "/home/mahyar/Desktop/test_photos"
+os.makedirs(save_path, exist_ok = True)
+
+looking_down = False # tack the look down state
+
 def estimate_head_pose(image):
+    global looking_down
+    
     # Convert image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
@@ -32,12 +52,32 @@ def estimate_head_pose(image):
         vertical_displacement = nose_tip[1] - eye_midpoint[1]
         
         # Determine if looking up or down based on vertical displacement
-        if vertical_displacement > 50:  # Adjust this threshold as needed
+        if vertical_displacement > 65:  # Adjust this threshold as needed
             print(f"Looking down: {vertical_displacement}")
+
+            # look down take the picture
+            if not looking_down:
+                filename = f"driver_{datetime.now().strftime('%Y%m%d_%H%M%S')}.jpg"
+                filepath = os.path.join(save_path, filename)
+                cv2.imwrite(filepath, image)
+                print(f"📸 Photo saved: {filepath}")
+                looking_down = True  # mark as captured so we don't spam
+            
+            print("Buzzer ON")
+            send_bit(0x01)
+
+
+
         elif vertical_displacement < 10:  # Adjust this threshold as needed
             print(f"Looking up: {vertical_displacement}")
+            looking_down = False
+            print("Buzzer ON")
+            send_bit(0x01)
         else:
             print(f"Looking straight: {vertical_displacement}")
+            looking_down = False
+            print("Buzzer OFF")
+            send_bit(0x00)
         
         # Draw facial landmarks for visualization
         for n in range(0, 68):
@@ -50,7 +90,7 @@ def estimate_head_pose(image):
 # Example usage
 cap = cv2.VideoCapture("/dev/v4l/by-id/usb-Ruision_UVC_Camera_20200416-video-index0")
 cap.set(cv2.CAP_PROP_FPS, 30)
-
+ 
 # open a video file or start a video stream 2
 cap2 = cv2.VideoCapture("/dev/v4l/by-id/usb-046d_Brio_100_2525APPPKBE8-video-index0")  # replace with 0 for webcam
 cap2.set(cv2.CAP_PROP_FPS, 30)
@@ -79,8 +119,18 @@ while True:
             results = model(frame2)
 
             for r in results:
-                frame2 = r.plot()
+                
+                # Only save if there are detections
+                if len(r.boxes) > 0:
+                    frame2 = r.plot()  # Annotated image
+                    filename = f"traffic_detected_{datetime.now().strftime('%Y%m%d_%H%M%S')}.jpg"
+                    filepath = os.path.join(save_path, filename)
+                    cv2.imwrite(filepath, frame2)
+                    print(f"📸 Saved annotated traffic light image: {filepath}")
+                else:
+                    print("No traffic light detected.")
 
+                    
         cv2.imshow("YOLO Camera", frame2)
 
         frame_id += 1
--- computerVision/run_parallelCV.sh
+++ computerVision/run_parallelCV.sh
@@ -2,7 +2,14 @@
 # Wait a few seconds to make sure desktop and camera services are ready
 sleep 10
 # Wait a few seconds for cameras to re-enumerate
-sleep 5
+sudo uhubctl -l 1 -p 1 -a off
+sudo uhubctl -l 3 -p 2 -a off
+sleep 1
+sudo uhubctl -l 1 -p 1 -a on
+sudo uhubctl -l 3 -p 2 -a on
+
+sleep 1
+
 cd /home/mahyar/Desktop/SD_CV_Demos
 source venv/bin/activate
 python parallelCV.py >> /home/mahyar/Desktop/SD_CV_Demos/camera_log.txt 2>&1
